8f874b863f317955aa7073a8f28455a1af781632
iman1234ahmadi <iman1234ahmadi@gmail.com>
2022-07-19 23:13:46 +0430

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+String name="iman ";
+
+    public static void main(String[] args) {
+        System.out.println("iman is ahmadi");
+	// write your code here
+    }
+}
